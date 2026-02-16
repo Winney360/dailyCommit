a2996c68ef427743edcda98e11e80f191cc1b8f5
Winney360 <nkathawinnie94@gmail.com>
2026-02-16 09:19:49 +0300

    fix: Add additional allowed origins for CORS configuration

--- server/index.js
+++ server/index.js
@@ -14,8 +14,10 @@ const log = console.log;
 // ------------------------ CORS ------------------------
 const allowedOrigins = [
   "http://localhost:5000",
+  "http://localhost:8081",
   "http://localhost:8183",
   "http://127.0.0.1:5000",
+  "http://127.0.0.1:8081",
   "http://127.0.0.1:8183",
 ];
 
