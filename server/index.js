57da3d3daae5e8f3ea8a3a0786b348f28cc24e9e
Winney360 <nkathawinnie94@gmail.com>
2026-02-22 11:50:57 +0300

    fix: Remove hardcoded CORS origins for improved flexibility

--- server/index.js
+++ server/index.js
@@ -25,15 +25,6 @@ const app = express();
 const log = console.log;
 
 // ------------------------ CORS ------------------------
-const allowedOrigins = [
-  "http://localhost:5000",
-  "http://localhost:8081",
-  "http://localhost:8183",
-  "http://127.0.0.1:5000",
-  "http://127.0.0.1:8081",
-  "http://127.0.0.1:8183",
-];
-
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (origin && allowedOrigins.includes(origin)) {
